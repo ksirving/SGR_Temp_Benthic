566f39efc35059f10a2f152f70a44a686fbf6f2e
ksirving <32795054+ksirving@users.noreply.github.com>
2022-07-29 08:46:25 -0700

    analysis and figure done for LAR temp tech memo

--- Code/temp_data_v4.R
+++ Code/temp_data_v4.R
@@ -183,18 +183,18 @@ airtempx <- airtemp %>%
   unite(col = "Date", c(Year, Month, Day), sep = "-") %>%
   mutate(DATE = as.Date(Date)) %>%
   filter(DATE %in% dates) %>% ## filter to water tep dates
-  mutate( Metric = "Max Air Temp") #%>% ## get mean from min max
+  mutate( Metric = "Max Air Temp") %>% ## get mean from min max
   select(DATE, TMAX) %>% rename( Date = DATE, AirTemp = TMAX) %>%
   mutate(AirTemp = (AirTemp -32)*5/9) #%>% ## convert to celcius
-  group_by( Year, Month, Day) #%>%
-  summarise(MeanTemp = mean(AirTemp),
-            MinTemp = min(AirTemp),
-            MaxTemp = max(AirTemp)) %>%
-  ungroup() %>%
-  # group_by(SiteName) %>%
-  mutate(max_07da = zoo::rollmean(MaxTemp, k=7, fill = NA)) %>% ## rolling daily max
-  mutate(mn_07da = zoo::rollmean(MeanTemp, k=7, fill = NA)) %>% ## rolling daily max
-  mutate(DTR = MaxTemp - MinTemp)
+  # group_by( Year, Month, Day) #%>%
+  # summarise(MeanTemp = mean(AirTemp),
+  #           MinTemp = min(AirTemp),
+  #           MaxTemp = max(AirTemp)) %>%
+  # ungroup() %>%
+  # # group_by(SiteName) %>%
+  # mutate(max_07da = zoo::rollmean(MaxTemp, k=7, fill = NA)) %>% ## rolling daily max
+  # mutate(mn_07da = zoo::rollmean(MeanTemp, k=7, fill = NA)) %>% ## rolling daily max
+  # mutate(DTR = MaxTemp - MinTemp)
 
 
   
@@ -225,37 +225,43 @@ head(df_date)
 range(na.omit(df_date$Date))
 names(df_date)
 
+
 ## alot going on here. join dates to airtemp preserving the na values, need the missing values to plot. make sites and metrics factors
 
-df_date <- df %>%
+
+df_datex <- df %>%
   unite(col = "Date", c(Year, Month, Day), sep = "-") %>%
   mutate(Date = as.Date(Date)) %>% rename(max07da = max_07da, mn07da = mn_07da) %>%
   pivot_longer(c(MeanTemp:DTR), names_to = "Metric", values_to = "Temp", values_drop_na = F) %>%
   unite("SiteMetric", c(SiteName, Metric), sep ="_", remove = T) %>%
-  pivot_wider(names_from =  SiteMetric, values_from = Temp) %>%
-  full_join(airtempx, by = "Date", keep = T)  %>% 
-  rename(Date = Date.x) %>% select(-Date.y) %>%
+  pivot_wider(names_from =  SiteMetric, values_from = Temp)%>%
+  right_join(airtempx, by = "Date", keep = T)%>% 
+  rename(Date = Date.y) %>% select(-Date.x) %>%
   pivot_longer(c(Benedict_MeanTemp:Willow_DTR), names_to = "SiteMetric", values_to = "Temp", values_drop_na = F) %>%
   separate(SiteMetric, into = c("SiteName", "Metric"), sep = "_") %>%
-  mutate(Metric = factor(Metric, levels = c("max07da", "mn07da", "MeanTemp", "MinTemp", "MaxTemp", "DTR"))) %>%
+  pivot_wider(names_from = Metric, values_from = Temp) %>% 
+  pivot_longer(c(AirTemp, MeanTemp:DTR), names_to="Metric", values_to = "Temp") %>%
+  mutate(Metric = factor(Metric, levels = c("max07da", "mn07da", "MeanTemp", "MinTemp", "MaxTemp", "DTR", "AirTemp"))) %>%
   mutate(SiteName = factor(SiteName, levels = c("Burbank", "Rattlesnake", "Benedict", "Steelhead", "Riverfront",
                                                 "Compton", "Willow")))
+head(df_datex)
+# test <- df_date %>% filter(SiteName == "Compton")
 
-unique(df_date$SiteName)
-
-test <- df_date %>% filter(SiteName == "Compton")
+library(ggthemes)
 
 ## plot
-T1 <- ggplot(df_date, aes(y = Temp, x = Date, group = Metric, color = Metric)) +
-  geom_line(aes(y=AirTemp)) +
+T1 <- ggplot(df_datex, aes(y = Temp, x = Date, group = Metric, color = Metric)) +
+  # geom_line(aes(y=AirTemp, col = "gray")) +
   geom_line() +
   # geom_line(aes(x=Date, y=AirTemp, color = "Max Daily Air Temp")) +
   facet_wrap(~SiteName) + ## , scales = "free_x"
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  scale_color_brewer(palette= "Accent",
+  scale_y_continuous(name="Water Temp (°C)") +
+  scale_color_brewer(palette= "Set2",
                        labels = c("7 Day Max", "7 Day Mean","Mean Daily Temp", "Min Daily Temp",
-                                  "Max Daily Temp", "Diurnal Temp Rate", "Max Air Temp")) +
-  scale_x_date(date_labels = "%b %Y")
+                                  "Max Daily Temp", "Diurnal Temp Rate", "Max Daily Air Temp")) +
+  scale_x_date(date_labels = "%b %Y") +
+  theme_linedraw() 
 
 
 T1
@@ -265,21 +271,24 @@ ggsave(T1, filename=file.name1, dpi=300, height=5, width=6)
 
 
 ## metric labels
-levels(df_date$Metric)
-levels(df_date$Metric) <- c("7 Day Max", "7 Day Mean","Mean Daily Temp", "Min Daily Temp",
+levels(df_datex$Metric)
+levels(df_datex$Metric) <- c("7 Day Max", "7 Day Mean","Mean Daily Temp", "Min Daily Temp",
                             "Max Daily Temp", "Diurnal Temp Rate", "AirTemp")
 
 ## plot
-T2 <- ggplot(subset(df_date, !Metric == "AirTemp"), aes(y = Temp, x = Date, group = SiteName, color = SiteName)) +
+T2 <- ggplot(subset(df_datex, !Metric == "AirTemp"), aes(y = Temp, x = Date, group = SiteName, color = SiteName)) +
   geom_line() +
   # geom_line(data = airtemp, aes(x=Date, y=Temp)) +
   facet_wrap(~Metric, scales = "free_x") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))+
   scale_x_date(date_labels = "%b %Y") +
+  scale_y_continuous(name="Water Temp (°C)") +
   # scale_color_manual(breaks = c("Burbank", "Rattlesnake", "Benedict", "Steelhead", "Riverfront",
   #                               "Compton","Willow"),
   #                 values=c("red", "blue", "green")) 
-  scale_color_brewer(palette= 'RdYlBu', direction=-1) 
+  scale_color_brewer(palette= 'RdYlBu', direction=-1)+
+  theme_dark()  
+
   # theme_dark()
 
 T2
@@ -299,11 +308,18 @@ library(leaflet)
 library(leafem)
 
 sites <- read.csv("Heal the Bay Temperature Logging Coordinates.csv")
-sites <- sites %>%
+sitesWater <- sites %>%
+  na.omit() %>%
   st_as_sf(coords=c("Longitude", "Latitude"), crs=4326, remove=F) %>%
   filter(!Site.Name %in% c("Rio Hondo Confluence", "Compton Creek Confluence"))
 
-sites
+# sitesAir <- sites %>%
+#   na.omit() %>%
+#   st_as_sf(coords=c("Longitude", "Latitude"), crs=4326, remove=F) %>%
+#   filter(Site.Name %in% c("LA Downtown"))
+# 
+# str(sitesWater)
+# sites
 # set background basemaps:
 basemapsList <- c( "Esri.WorldTopoMap" , "Esri.WorldImagery",
                   "Esri.NatGeoWorldMap",
@@ -313,16 +329,20 @@ basemapsList <- c( "Esri.WorldTopoMap" , "Esri.WorldImagery",
 mapviewOptions(basemaps=basemapsList, fgb = FALSE)
 # 
 # this map of all sites 
-m1 <- mapview(sites, col.regions = "orange",
-              layer.name="Temperature Loggers")  %>%
-  addStaticLabels(label = sites$Site.Name,
+m1 <- mapview(sitesWater, col.regions = "orange",
+                layer.name="Water Temperature Loggers") + #%>%
+ mapview(sitesAir, col.regions = "blue",
+          layer.name="Air Temperature Loggers")
+  
+
+m1 <- m1 %>% addStaticLabels(label = sitesWater$Site.Name,
                   # noHide = TRUE,
                   direction = 'top',
                   # textOnly = TRUE,
-                  textsize = "20px")
-# ?mapview
+                  textsize = "20px") 
 
 m1
+
 # m1@map %>% leaflet::addMeasure(primaryLengthUnit = "meters")
 
 mapshot(m1, url = paste0(getwd(), "/temp_logger_sites_mapview.html"),
@@ -334,20 +354,31 @@ mapshot(m1, url = paste0(getwd(), "/temp_logger_sites_mapview.html"),
 ## divide into concrete/trees - based on google earth
 
 head(df_date_type)
-unique(df_date_type$Metric)
 
-df_date_type <- df_date %>%
-  mutate(StreamType = case_when(SiteName %in% c("Burbank", "Benedict", "Steelhead") ~ "Vegetated",
-                   SiteName %in% c("Rattlesnake", "Compton", "Riverfront", "Willow") ~ "Concrete"))
+## remove air temp from metric list and add as sep clumn. Make names compitable with col names
+newnames <- make.names(names(df_date_type), unique = FALSE, allow_ = TRUE)
 
-?facet_grid
+df_date_type <- df_datex %>%
+  mutate(StreamType = case_when(SiteName %in% c("Burbank", "Benedict", "Steelhead", "Rattlesnake") ~ "Vegetated",
+                   SiteName %in% c("Compton", "Riverfront", "Willow") ~ "Concrete")) %>%
+  pivot_wider(names_from = Metric, values_from = Temp) 
+
+colnames(df_date_type) <- newnames
+
+df_date_type <- df_date_type %>%
+  pivot_longer(Mean.Daily.Temp:Diurnal.Temp.Rate, names_to = "Metric", values_to = "Temp") 
+
+levels(df_date_type$Metric) <- c("7 Day Max", "7 Day Mean","Mean Daily Temp", "Min Daily Temp",
+                               "Max Daily Temp", "Diurnal Temp Rate")
+
+head(df_date_type)
+
 ## plot
 T3 <- ggplot(df_date_type, aes(y = Temp, x = AirTemp, group = StreamType, color = StreamType)) +
   stat_smooth(method = "lm") +
   facet_wrap(~Metric) + ## , scales = "free_x"
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) #+
  
-
 
 T3
 
@@ -371,7 +402,7 @@ ggsave(T4, filename=file.name1, dpi=300, height=5, width=6)
 head(df_date_type)
 
 df_mod <- df_date_type %>%
-  filter(Metric == "Max Daily Temp")
+  filter(Metric == "Max.Daily.Temp")
 
 sites <- unique(df_mod$SiteName)
 sites
@@ -397,7 +428,7 @@ for(s in 1:length(sites)) {
   fitted_datax$AirTemp <- mod_lm$model$AirTemp ## air temp
   fitted_datax$SiteName <- sites[s] ## sites
   fitted_datax$R2 <- mod_sum$r.squared ## r 2
-  fitted_datax$PVal <- mod_sum$coefficients[4] ## p value
+  fitted_datax$PVal <-  mod_sum$coefficients[2,4] ## p value
   fitted_datax$n <- length(mod_lm$model$Temp)
   
   fitted_datax <- as.data.frame(fitted_datax)
@@ -408,11 +439,10 @@ for(s in 1:length(sites)) {
 
 head(fitted_data)
 
-#### stream type
-
+### add streamtype,  get positions for labels
 fitted_data <- fitted_data %>%
-  mutate(StreamType = case_when(SiteName %in% c("Burbank", "Benedict", "Steelhead") ~ "Vegetated",
-                                SiteName %in% c("Rattlesnake", "Compton", "Riverfront", "Willow") ~ "Concrete")) %>%
+  mutate(StreamType = case_when(SiteName %in% c("Burbank", "Benedict", "Steelhead", "Rattlesnake") ~ "Vegetated",
+                                SiteName %in% c("Compton", "Riverfront", "Willow") ~ "Concrete")) %>%
   group_by(SiteName) %>%
   mutate(Label = ifelse(Temp == max(Temp), paste(SiteName), NA))# %>%## add r2 for labels
   # mutate(Label = ifelse(length(na.omit(Label)) >= 2 , NA, Label))
@@ -421,7 +451,40 @@ fitted_data <- fitted_data %>%
 
 names(fitted_data)
 
+## plot
+T5a <- ggplot(fitted_data, aes(y = WaterTempFit, x = AirTemp, group = SiteName, color = SiteName)) +
+  stat_smooth(method = "lm") +
+  facet_wrap(~StreamType) + ## , scales = "free_x"
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_color_brewer(palette= 'Dark2', direction=-1) +
+  scale_y_continuous(name="Fitted Max Daily Water Temp (°C)") +
+  scale_x_continuous(name="Max Daily Air Temp (°C)") +
+  geom_label_repel(aes(label = Label),
+                   nudge_x = .75,
+                   na.rm = TRUE) +
+  theme(legend.position = "none")
+# theme_dark()
 
+
+
+T5a
+
+file.name1 <- paste0(out.dir, "Temp_vs_air_per_metric_fitted_site.jpg")
+ggsave(T5a, filename=file.name1, dpi=300, height=5, width=6)
+
+coefs <- fitted_data %>%
+  mutate(SiteName = factor(SiteName, levels = c("Burbank", "Rattlesnake", "Benedict", "Steelhead", "Riverfront",
+                                                "Compton", "Willow"))) %>%
+  dplyr::select(SiteName, StreamType, R2, PVal, n) %>%
+  distinct()
+
+coefs
+
+write.csv(coefs, "model_coefs_lm.csv")
+
+
+
+#### stream type
 sites <- unique(df_mod$StreamType)
 sites
 
@@ -438,15 +501,17 @@ for(s in 1:length(sites)) {
   ## model
   mod_lm <- lm(Temp ~ AirTemp, data = df_modx)
   mod_sum <- summary(mod_lm)
-  
+
   STfitted_datax <- NULL
   
+  mod_sum$coefficients[2,4]
+
   STfitted_datax$WaterTempFit <-  mod_lm$fitted.values ## water temp fit
   STfitted_datax$Temp <- mod_lm$model$Temp ## water temp in model
   STfitted_datax$AirTemp <- mod_lm$model$AirTemp ## air temp
   STfitted_datax$SiteName <- sites[s] ## sites
   STfitted_datax$R2 <- mod_sum$r.squared ## r 2
-  STfitted_datax$PVal <- mod_sum$coefficients[4] ## p value
+  STfitted_datax$PVal <- mod_sum$coefficients[2,4] ## p value
   STfitted_datax$n <- length(mod_lm$model$Temp)
   
   STfitted_datax <- as.data.frame(STfitted_datax)
@@ -487,13 +552,13 @@ T5 <- ggplot(STfitted_data, aes(y = WaterTempFit, x = AirTemp, group = SiteName,
                    nudge_x = .75,
                    na.rm = TRUE) +
   theme(legend.position = "none")
-  # theme_dark()
+# theme_dark()
 
 
 
 T5
 
-file.name1 <- paste0(out.dir, "Temp_vs_air_per_metric_fitted_streamtyoe.jpg")
+file.name1 <- paste0(out.dir, "Temp_vs_air_per_metric_fitted_streamtype.jpg")
 ggsave(T5, filename=file.name1, dpi=300, height=5, width=6)
 
 coefs <- STfitted_data %>%
@@ -507,37 +572,8 @@ coefs
 
 write.csv(coefs, "model_coefs_lm_streamType.csv")
 
-### stream type
-
-T5a <- ggplot(STfitted_data, aes(y = WaterTempFit, x = AirTemp, group = StreamType, color = StreamType)) +
-  stat_smooth(method = "lm") +
-  # facet_wrap(~StreamType) + ## , scales = "free_x"
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  scale_color_brewer(palette= 'Dark2', direction=-1) +
-  scale_y_continuous(name="Fitted Max Daily Water Temp (°C)") +
-  scale_x_continuous(name="Max Daily Air Temp (°C)") +
-  geom_label_repel(aes(label = Label),
-                   nudge_x = .75,
-                   na.rm = TRUE) +
-  theme(legend.position = "none")
-# theme_dark()
 
 
-
-T5a
-
-file.name1 <- paste0(out.dir, "Temp_vs_air_per_metric_fitted_StreamType.jpg")
-ggsave(T5a, filename=file.name1, dpi=300, height=5, width=6)
-
-coefs <- fitted_data %>%
-  mutate(SiteName = factor(SiteName, levels = c("Burbank", "Rattlesnake", "Benedict", "Steelhead", "Riverfront",
-                                                "Compton", "Willow"))) %>%
-  dplyr::select(SiteName, StreamType, R2, PVal, n) %>%
-  distinct()
-
-coefs
-
-write.csv(coefs, "model_coefs_lm.csv")
 
 # fitted_data <- fitted_data %>%
 #   mutate(StreamType = case_when(SiteName %in% c("Burbank", "Benedict", "Steelhead") ~ "Natural",
@@ -574,7 +610,7 @@ write.csv(coefs, "model_coefs_lm.csv")
 
 head(df_date_type)
 
-T7 <- ggplot(data = subset(df_date_type, Metric == "Diurnal Temp Rate") , aes(x = SiteName, y = Temp, fill = StreamType)) +
+T7 <- ggplot(data = subset(df_date_type, Metric == "Diurnal.Temp.Rate") , aes(x = SiteName, y = Temp, fill = StreamType)) +
   geom_boxplot() +
   scale_x_discrete(name = "Site") +
   scale_y_continuous(name = "Diurnal Temp Rate (°C)")
@@ -584,7 +620,7 @@ T7
 file.name1 <- paste0(out.dir, "Temp_diurnal_rate.jpg")
 ggsave(T7, filename=file.name1, dpi=300, height=5, width=6)
 
-T7a <- ggplot(data = subset(df_date_type, Metric == "Diurnal Temp Rate") , aes(x = StreamType, y = Temp, fill = StreamType)) +
+T7a <- ggplot(data = subset(df_date_type, Metric == "Diurnal.Temp.Rate") , aes(x = StreamType, y = Temp, fill = StreamType)) +
   geom_boxplot() +
   scale_x_discrete(name = "Site") +
   scale_y_continuous(name = "Diurnal Temp Rate (°C)")
@@ -607,7 +643,7 @@ which(is.na(df_date_seas))
 
 df_date_seas[9871:9876,]
 
-T7b <- ggplot(data = subset(df_date_seas, Metric == "Diurnal Temp Rate") , aes(x = Season, y = Temp, fill = StreamType)) +
+T7b <- ggplot(data = subset(df_date_seas, Metric == "Diurnal.Temp.Rate") , aes(x = Season, y = Temp, fill = StreamType)) +
   geom_boxplot() +
   # facet_wrap(~StreamType) +
   scale_x_discrete(name = "Season") +
@@ -618,7 +654,7 @@ T7b
 file.name1 <- paste0(out.dir, "Temp_diurnal_rate_season_streamtype.jpg")
 ggsave(T7b, filename=file.name1, dpi=300, height=5, width=6)
 
-T7c <- ggplot(data = subset(df_date_seas, Metric == "Diurnal Temp Rate") , aes(x = Season, y = Temp, fill = Season)) +
+T7c <- ggplot(data = subset(df_date_seas, Metric == "Diurnal.Temp.Rate") , aes(x = Season, y = Temp, fill = Season)) +
   geom_boxplot() +
   # facet_wrap(~StreamType) +
   scale_x_discrete(name = "Season") +
